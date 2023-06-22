8ca3e4a9e953717a10744dae628ea32bf4ce215a
Adrián Montemayor Rojas <adrianmontemayor.r@gmail.com>
2023-06-22 11:44:12 -0600

    Project Items Displaying

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import "../styles/Projects.css";
+import ProjectItem from "../components/ProjectItem";
+import Colorpalettepic from "../assets/colorpalette.png";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div className="projects">
+      <h1>My personal projects</h1>
+      <div className="projectList">
+        <ProjectItem name="Color palette generator" image={Colorpalettepic} />
+        <ProjectItem name="Color palette generator" image={Colorpalettepic} />
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
